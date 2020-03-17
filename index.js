13b93376a1aac22accfb08f328073ad5c80620d0
T.Jay Rocket <tjayrocket@gmail.com>
2020-03-17 09:19:14 -0700

    First lesson for Xavior

---
+++ index.js
@@ -0,0 +1,22 @@
+'use strict';
+
+console.log('This is the console. You can do anything you want to a page from here.');
+
+console.log('You can call any function in the console. Be it one you wrote, or one that node enables. Just type \'greet()\' into the console and press enter.');
+
+function greet(){
+  alert('Hey X! Look up here! Press okay to get rid of this!');
+  console.log('Alerts happen from time to time... though they are considered bad practice. Now type \'message()\' into the console, press enter and see what happens...');
+}
+
+function message(){
+  console.log('All kinds of things are hidden in the console for all kinds of websites. I want you to bring up old.reddit.com and press F12 to access the console and tell me what you see... do the same thing to facebook. After that, put in the command \'next()\' and follow those instructions - don\'t close this page though!');
+}
+
+function next(){
+  console.log('As you can see, some websites give you hidden messages or warnings about their site. The console is actually an extremely good place to play with code and see what it does and learn from the errors. Nothing you do there is permanent and refreshing the page will undo any changes you made. Even on this page, you can add things to the page, change the text and more. It will only exist on your computer and no where else. Later, I will show you how to save your work on your machine so that you don\'t lose your work and the website will be permanently altered on your computer only. This is an excellent way to fuck with people by the way... but we\'ll discuss that later. For now type \'final()\' into the console and let it rip...');
+}
+
+function final(){
+  alert('That is it for today! Keep reading, go to your favorite website and fuck around in the console... read whats happening, see if anything is hidden out there. You can do it to EVERY WEBSITE! We\'ll talk more later kid.');
+}
